b1d62c2a5994cde70651f289d820c98dd27efba1
emkiusz <mkiszka525@gmail.com>
2020-06-27 04:00:27 +0200

    Prototype AI

--- Form1.cs
+++ Form1.cs
@@ -14,195 +14,21 @@ namespace tictactoeAI
     public partial class Form1 : Form
     {
         Random rand = new Random();
-        String testZapis = "";
         int randomNum;
         int[] plansza = new int[] {0,0,0,0,0,0,0,0,0};
+        PictureBox[] poleGracza;
         String planszaZapis = "";
-        int tura = 1;
+        int czyjaTura = 1;
 
         public Form1()
         {
             InitializeComponent();
+            poleGracza = new PictureBox[] { pole1, pole2, pole3, pole4, pole5, pole6, pole7, pole8, pole9 };
         }
-
-        private void pole1_Click(object sender, EventArgs e)
-        {
-            if (tura == 1)
-            {
-                if (plansza[0] == 0)
-                {
-                    pole1.Image = Properties.Resources.o;
-                }
-                plansza[0] = 1;
-                if (tura < 3)
-                {
-                    turaKomputera();
-                }
-                else
-                {
-                    button1.Visible = true;
-                }
-            }
-        }
-
-        private void pole2_Click(object sender, EventArgs e)
-        {
-            if (tura == 1)
-            {
-                if (plansza[1] == 0)
-                {
-                    pole2.Image = Properties.Resources.o;
-                }
-                plansza[1] = 1;
-                if (tura < 3)
-                {
-                    turaKomputera();
-                }
-                else
-                {                    
-                    button1.Visible = true;
-                }
-            }
-        }
-
-        private void pole3_Click(object sender, EventArgs e)
-        {
-            if (tura == 1)
-            {
-                if (plansza[2] == 0)
-                {
-                    pole3.Image = Properties.Resources.o;
-                }
-                plansza[2] = 1;
-                if (tura < 3)
-                {
-                    turaKomputera();
-                }
-                else
-                {
-                    button1.Visible = true;
-                }
-            }
-        }
-
-        private void pole4_Click(object sender, EventArgs e)
-        {
-            if (tura == 1)
-            {
-                if (plansza[3] == 0)
-                {
-                    pole4.Image = Properties.Resources.o;
-                }
-                plansza[3] = 1;
-                if (tura < 3)
-                {
-                    turaKomputera();
-                }
-                else
-                {
-                    button1.Visible = true;
-                }
-            }
-        }
-
-        private void pole5_Click(object sender, EventArgs e)
-        {
-            if (tura == 1)
-            {
-                if (plansza[4] == 0)
-                {
-                    pole5.Image = Properties.Resources.o;
-                }
-                plansza[4] = 1;
-                if (tura < 3)
-                {
-                    turaKomputera();
-                }
-                else
-                {
-                    button1.Visible = true;
-                }
-            }
-        }
-
-        private void pole6_Click(object sender, EventArgs e)
-        {
-            if (tura == 1)
-            {
-                if (plansza[5] == 0)
-                {
-                    pole6.Image = Properties.Resources.o;
-                }
-                plansza[5] = 1;
-                if (tura < 3)
-                {
-                    turaKomputera();
-                }
-                else
-                {
-                    button1.Visible = true;
-                }
-            }
-        }
-
-        private void pole7_Click(object sender, EventArgs e)
-        {
-            if (tura == 1)
-            {
-                if (plansza[6] == 0)
-                {
-                    pole7.Image = Properties.Resources.o;
-                }
-                plansza[6] = 1;
-                if (tura < 3)
-                {
-                    turaKomputera();
-                }
-                else
-                {
-                    button1.Visible = true;
-                }
-            }
-        }
-
-        private void pole8_Click(object sender, EventArgs e)
-        {
-            if (tura == 1)
-            {
-                if (plansza[7] == 0)
-                {
-                    pole8.Image = Properties.Resources.o;
-                }
-                plansza[7] = 1;
-                if (tura < 3)
-                {
-                    turaKomputera();
-                }
-                else
-                {
-                    button1.Visible = true;
-                }
-            }
-        }
-
-        private void pole9_Click(object sender, EventArgs e)
-        {
-            if (tura == 1)
-            {
-                if (plansza[8] == 0)
-                {
-                    pole9.Image = Properties.Resources.o;
-                }
-                plansza[8] = 1;                
-                if (tura < 3)
-                {
-                    turaKomputera();
-                }
-            }
-        }        
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            planszaZapis = "";
             if (!File.Exists("przegrana.txt"))
             {
                 using (StreamWriter sw = File.CreateText("przegrana.txt"))
@@ -219,67 +45,196 @@ namespace tictactoeAI
             }
         }
 
-        public void turaKomputera()
+        #region klikanie gracza
+        private void pole1_Click(object sender, EventArgs e)
         {
-            tura = 0;
-            label3.Text = "Tura komputera.";
-            do
-            {
-                randomNum = rand.Next(9);
-            }
-            while (plansza[randomNum] != 0);
-            plansza[randomNum] = 2;
-            switch (randomNum)
-            {
-                case 0:
-                    pole1.Image= Properties.Resources.x;
-                    break;
-                case 1:
-                    pole2.Image = Properties.Resources.x;
-                    break;
-                case 2:
-                    pole3.Image = Properties.Resources.x;
-                    break;
-                case 3:
-                    pole4.Image = Properties.Resources.x;
-                    break;
-                case 4:
-                    pole5.Image = Properties.Resources.x;
-                    break;
-                case 5:
-                    pole6.Image = Properties.Resources.x;
-                    break;
-                case 6:
-                    pole7.Image = Properties.Resources.x;
-                    break;
-                case 7:
-                    pole8.Image = Properties.Resources.x;
-                    break;
-                case 8:
-                    pole9.Image = Properties.Resources.x;
-                    break;
-            }
-            sprawdzWygrana();
-            if (tura < 3)
-            {
-                label3.Text = "Tura gracza.";
-                tura = 1;
-            }
+            turaGracza(0);
         }
 
+        private void pole2_Click(object sender, EventArgs e)
+        {
+            turaGracza(1);
+        }
+
+        private void pole3_Click(object sender, EventArgs e)
+        {
+            turaGracza(2);
+        }
+
+        private void pole4_Click(object sender, EventArgs e)
+        {
+            turaGracza(3);
+        }
+
+        private void pole5_Click(object sender, EventArgs e)
+        {
+            turaGracza(4);
+        }
+
+        private void pole6_Click(object sender, EventArgs e)
+        {
+            turaGracza(5);
+        }
+
+        private void pole7_Click(object sender, EventArgs e)
+        {
+            turaGracza(6);
+        }
+
+        private void pole8_Click(object sender, EventArgs e)
+        {
+            turaGracza(7);
+        }
+
+        private void pole9_Click(object sender, EventArgs e)
+        {
+            turaGracza(8);
+        }
+        #endregion
+
+        #region tura komputera
+        public void turaKomputera()
+        {
+            String tempLine = "";
+            int kolejnyRuch=-1;
+            int wygrana = -1;
+
+            label3.Text = "Tura komputera.";
+
+            if (czyjaTura < 3)
+            {
+                czyjaTura = 0;
+
+                foreach (var line in File.ReadLines("przegrana.txt"))
+                {
+                    if (line != "")
+                    {
+                        tempLine = line.Substring(0, planszaZapis.Length);
+                        if (tempLine.Contains(planszaZapis.Substring(0, planszaZapis.Length)))
+                        {
+                            MessageBox.Show("Następny ruch, aby nie przegrać: " + line.Substring(planszaZapis.Length, 1));
+                            kolejnyRuch = int.Parse(line.Substring(planszaZapis.Length, 1));
+                            wygrana = 0;
+                            break;
+                        }
+                    }
+                }
+
+                if (wygrana != 0)
+                {
+                    foreach (var line in File.ReadLines("wygrana.txt"))
+                    {
+                        if (line != "")
+                        {
+                            tempLine = line.Substring(0, planszaZapis.Length);
+                            if (tempLine.Contains(planszaZapis.Substring(0, planszaZapis.Length)))
+                            {
+                                MessageBox.Show("Następny ruch, aby wygrać: " + line.Substring(planszaZapis.Length, 1));
+                                kolejnyRuch = int.Parse(line.Substring(planszaZapis.Length, 1));
+                                wygrana = 1;
+                                break;
+                            }
+                        }
+                    }
+                }
+                                
+                if (wygrana != 1)
+                {
+                    if (wygrana == -1)
+                    {
+                        do
+                        {
+                            randomNum = rand.Next(9);
+                        }
+                        while (plansza[randomNum] != 0);
+                    }
+                    if(wygrana==0)
+                    {
+                        do
+                        {
+                            randomNum = rand.Next(9);
+                        }
+                        while (plansza[randomNum] != 0 && randomNum!=kolejnyRuch);
+                    }
+                }
+                else
+                {
+                    randomNum = kolejnyRuch;
+                }
+                planszaZapis += randomNum;
+                plansza[randomNum] = 2;
+                switch (randomNum)
+                {
+                    case 0:
+                        pole1.Image = Properties.Resources.x;
+                        break;
+                    case 1:
+                        pole2.Image = Properties.Resources.x;
+                        break;
+                    case 2:
+                        pole3.Image = Properties.Resources.x;
+                        break;
+                    case 3:
+                        pole4.Image = Properties.Resources.x;
+                        break;
+                    case 4:
+                        pole5.Image = Properties.Resources.x;
+                        break;
+                    case 5:
+                        pole6.Image = Properties.Resources.x;
+                        break;
+                    case 6:
+                        pole7.Image = Properties.Resources.x;
+                        break;
+                    case 7:
+                        pole8.Image = Properties.Resources.x;
+                        break;
+                    case 8:
+                        pole9.Image = Properties.Resources.x;
+                        break;
+                }
+                sprawdzWygrana();
+                if (czyjaTura < 3)
+                {
+                    label3.Text = "Tura gracza.";
+                    czyjaTura = 1;
+                }
+            }
+        }
+        #endregion
+
+        #region tura gracza
+        public void turaGracza(int pole)
+        {
+            if (czyjaTura == 1)
+            {
+                if (plansza[pole] == 0)
+                {
+                    planszaZapis += pole;
+                    poleGracza[pole].Image = Properties.Resources.o;
+                    plansza[pole] = 1;
+                    sprawdzWygrana();
+                    if (czyjaTura < 3)
+                    {
+                        turaKomputera();
+                    }
+                }
+            }
+        }
+        #endregion
         public void sprawdzWygrana()
         {
-            testZapis = "";
-            foreach (int x in plansza)
+            MessageBox.Show(planszaZapis.Length.ToString());
+            String temp = "";
+            foreach(int z in plansza)
             {
-                testZapis += x.ToString();
+                temp += z.ToString();
             }
-
-            if(!testZapis.Contains("0"))
+            if(!temp.Contains("0"))
             {                
                 label3.Text = "Remis";
                 button1.Visible = true;
-                tura = 5;
+                czyjaTura = 5;
             }
             else
             {
@@ -290,17 +245,24 @@ namespace tictactoeAI
                 (plansza[2] == 1 && plansza[4] == 1 && plansza[6] == 1) ||
                 (plansza[3] == 1 && plansza[4] == 1 && plansza[5] == 1) ||
                 (plansza[0] == 1 && plansza[3] == 1 && plansza[6] == 1) ||
-                (plansza[2] == 1 && plansza[5] == 1 && plansza[8] == 1)
+                (plansza[2] == 1 && plansza[5] == 1 && plansza[8] == 1) ||
+                (plansza[6] == 1 && plansza[7] == 1 && plansza[8] == 1)
               )
                 {
-                    foreach (int x in plansza)
+                    if (planszaZapis.Length != 9)
                     {
-                        planszaZapis += x.ToString();
+                        int z = 0;
+                        int temp2 = 9 - planszaZapis.Length;
+                        while (z < temp2)
+                        {
+                            planszaZapis += 0;
+                            z++;
+                        }
                     }
                     File.AppendAllText("przegrana.txt", planszaZapis + Environment.NewLine);              
                     label3.Text = "Wygrał gracz.";
                     button1.Visible = true;
-                    tura=3;
+                    czyjaTura=3;
                 }
 
                 if (
@@ -310,19 +272,26 @@ namespace tictactoeAI
                     (plansza[2] == 2 && plansza[4] == 2 && plansza[6] == 2) ||
                     (plansza[3] == 2 && plansza[4] == 2 && plansza[5] == 2) ||
                     (plansza[0] == 2 && plansza[3] == 2 && plansza[6] == 2) ||
-                    (plansza[2] == 2 && plansza[5] == 2 && plansza[8] == 2)
+                    (plansza[2] == 2 && plansza[5] == 2 && plansza[8] == 2) ||
+                    (plansza[6] == 2 && plansza[7] == 2 && plansza[8] == 2)
                   )
-                {
-                    foreach (int x in plansza)
+                {                    
+                    if(planszaZapis.Length!=9)
                     {
-                        planszaZapis += x.ToString();
+                        int z = 0;
+                        int temp2 = 9 - planszaZapis.Length;
+                        while(z<temp2)
+                        {
+                            planszaZapis += 0;
+                            z++;
+                        }
                     }
                     File.AppendAllText("wygrana.txt", planszaZapis + Environment.NewLine);
                     label3.Text = "Wygrał komputer.";
                     button1.Visible = true;
-                    tura =4;
+                    czyjaTura =4;
                 }
-            }            
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
